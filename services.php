29fc9fd4b8fef2bdb97d87e216d38ab787389b35
Marlen Roberts <roberts.marlen@gmail.com>
2017-11-18 00:09:47 -0500

    Updated service page styles

--- services.php
+++ services.php
@@ -16,7 +16,7 @@
             <p>Diversity D can provide you with everything necessary for a drip irrgation system, including an assortment of drip tape, filters, pumps, pipe and fittings, all the way to complete systems. We will work with you to design your system and coordinate installation. Click <a href="https://docs.google.com/forms/d/1L1LofH7ASTOKWcGLRDqVCNNq4Fw5nmcP3WpkSL8TUsM/viewform?usp=send_form" target="_blank">here</a> to fill out an online form, or <a href="mailto:sales@diversityd.com">contact</a> us today for more information. </p>
             <div style="float: left; width: 50%;">
                 <h2>System Components</h2>
-                <ul>
+                <ul class="vlist">
                     <li>Thinwall Dripline</li>
                     <li>Heavywall Dripline</li>
                     <li>Polyethylene Tubing</li>
@@ -28,21 +28,20 @@
                     <li>Crop Management Technologies</li>
                     <li>Automated Systems</li>
                 </ul>
-                <h3>Additional Products </h3>
-                <ul>
+                <h3>Additional Products</h3>
+                <ul class="vlist">
                     <li><a href="http://www.inventiveagproducts.com/verm_descript.html" target="_blank">Verminator Gopher Control Authorized Dealer</a></li>
                     <li><a href="http://www.irripod.com/" target="_blank">IrriPod Flexible Irrigation</a></li>
                     <li><a href="http://www.aquaspy.com/" target="_blank">AquaSpy</a></li>
                     <li><a href="http://www.agsense.net/" target="_blank">AgSense</a></li>
                 </ul>
             </div>
-            <div style="margin-left: 50%;">
-            <h2>Drip Irrigation</h2>
-                <p>We specialize in servicing all brands of new and existing drip irrigation systems to keep your operation running smoothly. Contact us today to request service on a system. </p>
-                <h2>Other Irrigation</h2>
+            <div style="margin-left: 50%; margin-bottom: -100px;">
+                <h2>Drip Irrigation</h2>
+                <p>We specialize in servicing all brands of new and existing drip irrigation systems to keep your operation running smoothly. Contact us today to request service on a system.</p>
                 <p>Other Irrigation: We service all makes and models of center pivots, traveling sprinklers, solid set irrigation and flexible irrigation.</p>
                 <h3>Services</h3>
-                <ul>
+                <ul class="vlist">
                     <li>Complete Drip System installation</li>
                     <li>Winterization</li>
                     <li>System start up</li>
@@ -62,20 +61,75 @@
             </div>
             <h3>Dripline </h3>
             <p>
-                <a href="http://www.netafimusa.com/agriculture/products/thinwall" target="_blank" onMouseOver="MM_swapImage('Dripline','','coilRollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/coil.gif" alt="Dripline" name="Dripline" width="200" height="200" border="0" class="fltlt"></a>
-                <a href="http://www.netafimusa.com/agriculture/products/hwdripperline" target="_blank" onMouseOver="MM_swapImage('Heavywall Dripline','','coil2Rollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/coil2.gif" alt="Heavywall Dripline" name="Heavywall Dripline" width="200" height="200" border="0" class="fltlt"></a>
+                <a class="swap" href="http://www.netafimusa.com/agriculture/products/thinwall" target="_blank">
+                    <img src="/images/coil.gif" alt="Dripline" name="Dripline" width="200" height="200" border="0">
+                    <img src="/images/coilRollover.gif" alt="Dripline" name="Dripline" width="200" height="200" border="0">
+                </a>
+                <a class="swap" href="http://www.netafimusa.com/agriculture/products/hwdripperline" target="_blank">
+                    <img src="/images/coil2.gif" alt="Heavywall Dripline" name="Heavywall Dripline" width="200" height="200" border="0">
+                    <img src="/images/coil2Rollover.gif" alt="Heavywall Dripline" name="Heavywall Dripline" width="200" height="200" border="0">
+                </a>
             </p>
             <h3>Filters</h3>
-            <p><a href="http://www.netafimusa.com/agriculture/products/steel-media-filters" target="_blank" onMouseOver="MM_swapImage('Single Chamber Tank','','singlechambertankRollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/singlechambertank.gif" alt="Single Chamber Tank" name="Single Chamber Tank" width="200" height="200" border="0" class="fltlt"></a></p>
-            <p><a href="http://www.netafimusa.com/agriculture/products/filters" target="_blank" onMouseOver="MM_swapImage('Disc Kleen Filters','','3discRollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/3disc.gif" alt="Disc Kleen Filters" name="Disc Kleen Filters" width="200" height="200" border="0" class="fltlt"></a><a href="http://www.netafimusa.com/agriculture/products/sand-separators" target="_blank" onMouseOver="MM_swapImage('Hydrocyclone and Sedimentation Tank','','sedimentationtankRollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/sedimentationtank.gif" alt="Hydrocyclone and Sedimentation Tank" name="Hydrocyclone and Sedimentation Tank" width="200" height="200" border="0" class="fltlt"></a></p>
-            <p><a href="http://www.netafimusa.com/agriculture/products/csfilters" target="_blank" onMouseOver="MM_swapImage('Screen Filter','','screenfilterRollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/screenfilter.gif" alt="Screen Filter" name="Screen Filter" width="200" height="200" border="0" class="fltlt1"></a></p>
+            <p>
+                <a class="swap" href="http://www.netafimusa.com/agriculture/products/steel-media-filters" target="_blank">
+                    <img src="/images/singlechambertank.gif" alt="Single Chamber Tank" name="Single Chamber Tank" width="200" height="200" border="0">
+                    <img src="/images/singlechambertankRollover.gif" alt="Single Chamber Tank" name="Single Chamber Tank" width="200" height="200" border="0">
+                </a>
+                <a class="swap" href="http://www.netafimusa.com/agriculture/products/filters" target="_blank">
+                    <img src="/images/3disc.gif" alt="Disc Kleen Filters" name="Disc Kleen Filters" width="200" height="200" border="0">
+                    <img src="/images/3discRollover.gif" alt="Disc Kleen Filters" name="Disc Kleen Filters" width="200" height="200" border="0">
+                </a>
+                <a class="swap" href="http://www.netafimusa.com/agriculture/products/sand-separators" target="_blank">
+                    <img src="/images/sedimentationtank.gif" alt="Hydrocyclone and Sedimentation Tank" name="Hydrocyclone and Sedimentation Tank" width="200" height="200" border="0">
+                    <img src="/images/sedimentationtankRollover.gif" alt="Hydrocyclone and Sedimentation Tank" name="Hydrocyclone and Sedimentation Tank" width="200" height="200" border="0">
+                </a>
+                <a class="swap" href="http://www.netafimusa.com/agriculture/products/csfilters" target="_blank">
+                    <img src="/images/screenfilter.gif" alt="Screen Filter" name="Screen Filter" width="200" height="200" border="0">
+                    <img src="/images/screenfilterRollover.gif" alt="Screen Filter" name="Screen Filter" width="200" height="200" border="0">
+                </a>
+            </p>
             <h3>Air Vents, Valves and Pressure Regulators</h3>
-            <p><a href="http://www.netafimusa.com/agriculture/products/battery-operated-control-valves" target="_blank" onMouseOver="MM_swapImage('Valve Controller','','valve1Rollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/valve1.gif" alt="Valve Controller" name="Valve Controller" width="200" height="200" border="0" class="fltlt"></a><a href="http://www.netafimusa.com/agriculture/products/nylon-pvc-valves" target="_blank" onMouseOver="MM_swapImage('Valve','','valve2Rollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/valve2.gif" alt="Valve" name="Valve" width="200" height="200" border="0" class="fltlt"></a><a href="http://www.netafimusa.com/agriculture/products/checkvalves-airvents-pressureregulators" target="_blank" onMouseOver="MM_swapImage('Air Vent','','airventRollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/airvent.gif" alt="Air Vent" name="Air Vent" width="200" height="200" border="0" class="fltlt"></a><a href="http://www.netafimusa.com/agriculture/products/airvents-combination" target="_blank" onMouseOver="MM_swapImage('Air Vent with Shrader','','airvent2Rollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/airvent2.gif" alt="Air Vent with Shrader" name="Air Vent with Shrader" width="200" height="200" border="0"></a><a href="http://www.netafimusa.com/agriculture/products/pressureregulators" target="_blank" onMouseOver="MM_swapImage('Regulator','','regulatorRollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/regulator.gif" alt="Regulator" name="Regulator" width="200" height="200" border="0"></a></p>
-            <p><a href="http://www.netafimusa.com/agriculture/products/pressureregulators" target="_blank" class="fltrt" onMouseOver="MM_swapImage('Regulator','','regulatorRollover.gif',1)" onMouseOut="MM_swapImgRestore()"><a href="http://www.netafimusa.com/agriculture/products/airvents-combination" target="_blank" class="fltrt" onMouseOver="MM_swapImage('Air Vent with Shrader','','airvent2Rollover.gif',1)" onMouseOut="MM_swapImgRestore()"></a></p>
+            <p>
+                <a class="swap" href="http://www.netafimusa.com/agriculture/products/battery-operated-control-valves" target="_blank">
+                    <img src="/images/valve1.gif" alt="Valve Controller" name="Valve Controller" width="200" height="200" border="0">
+                    <img src="/images/valve1Rollover.gif" alt="Valve Controller" name="Valve Controller" width="200" height="200" border="0">
+                </a>
+                <a class="swap" href="http://www.netafimusa.com/agriculture/products/nylon-pvc-valves" target="_blank">
+                    <img src="/images/valve2.gif" alt="Valve" name="Valve" width="200" height="200" border="0">
+                    <img src="/images/valve2Rollover.gif" alt="Valve" name="Valve" width="200" height="200" border="0">
+                </a>
+                <a class="swap" href="http://www.netafimusa.com/agriculture/products/checkvalves-airvents-pressureregulators" target="_blank">
+                    <img src="/images/airvent.gif" alt="Air Vent" name="Air Vent" width="200" height="200" border="0">
+                    <img src="/images/airventRollover.gif" alt="Air Vent" name="Air Vent" width="200" height="200" border="0">
+                </a>
+                <a class="swap" href="http://www.netafimusa.com/agriculture/products/airvents-combination" target="_blank">
+                    <img src="/images/airvent2.gif" alt="Air Vent with Shrader" name="Air Vent with Shrader" width="200" height="200" border="0">
+                    <img src="/images/airvent2Rollover.gif" alt="Air Vent with Shrader" name="Air Vent with Shrader" width="200" height="200" border="0">
+                </a>
+                <a class="swap" href="http://www.netafimusa.com/agriculture/products/pressureregulators" target="_blank">
+                    <img src="/images/regulator.gif" alt="Regulator" name="Regulator" width="200" height="200" border="0">
+                    <img src="/images/regulatorRollover.gif" alt="Regulator" name="Regulator" width="200" height="200" border="0">
+                </a>
+            </p>
             <h3>Water Meters</h3>
-            <p><a href="http://www.netafimusa.com/agriculture/products/watermeters-octave" target="_blank" onMouseOver="MM_swapImage('Octave','','octaveRollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/octave.gif" alt="Octave Water Meter" name="Octave" width="200" height="200" border="0" class="fltlt"></a></p>
+            <p>
+                <a class="swap" href="http://www.netafimusa.com/agriculture/products/watermeters-octave" target="_blank">
+                    <img src="/images/octave.gif" alt="Octave Water Meter" name="Octave" width="200" height="200" border="0">
+                    <img src="/images/octaveRollover.gif" alt="Octave Water Meter" name="Octave" width="200" height="200" border="0">
+                </a>
+            </p>
             <h3>Controllers</h3>
-            <p><a href="http://bermad.com/usa/product/product=bic-2000-redio-system-guid-ir/cat=91794" target="_blank" onMouseOver="MM_swapImage('Irrigation Controllers','','bic2500Rollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/bic2500.gif" alt="Irrigation Controllers" name="Irrigation Controllers" width="200" height="200" border="0" class="fltlt"></a><a href="http://www.bermad.com/usa/product/product=bic-_1000_controller/cat=83435" target="_blank" onMouseOver="MM_swapImage('Bic 1000 Controller','','bic1000Rollover.gif',1)" onMouseOut="MM_swapImgRestore()"><img src="/images/bic1000.gif" alt="Bic 1000 Controller" name="Bic 1000 Controller" width="200" height="200" border="0"></a></p>
+            <p>
+                <a class="swap" href="http://bermad.com/usa/product/product=bic-2000-redio-system-guid-ir/cat=91794" target="_blank">
+                    <img src="/images/bic2500.gif" alt="Irrigation Controllers" name="Irrigation Controllers" width="200" height="200" border="0">
+                    <img src="/images/bic2500Rollover.gif" alt="Irrigation Controllers" name="Irrigation Controllers" width="200" height="200" border="0">
+                </a>
+                <a class="swap" href="http://www.bermad.com/usa/product/product=bic-_1000_controller/cat=83435" target="_blank">
+                    <img src="/images/bic1000.gif" alt="Bic 1000 Controller" name="Bic 1000 Controller" width="200" height="200" border="0">
+                    <img src="/images/bic1000Rollover.gif" alt="Bic 1000 Controller" name="Bic 1000 Controller" width="200" height="200" border="0">
+                </a>
+            </p>
         </div>
         <?php include __DIR__ . '/templates/footer.html'; ?>
     </div>
